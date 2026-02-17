71e4ebcf553c0086dfe2b4ad5087c0469f48d721
Ihsan Nur Faqih <ihsan_nurfaqih@apps.ipb.ac.id>
2026-02-17 11:46:41 +0700

    sql script to answer hw04

---
+++ hw04/hw04.sql
@@ -0,0 +1,26 @@
+-- Total records of fct_monthly_zone_revenue
+SELECT
+	count(*)
+FROM taxi_rides_ny.analytics_prod.fct_monthly_zone_revenue;
+
+-- Zone with the highest revenue for Green taxis in 2020
+select
+	pickup_zone
+	, sum(revenue_monthly_total_amount) as total_revenue
+from taxi_rides_ny.analytics_prod.fct_monthly_zone_revenue
+where true
+	and service_type = 'Green'
+	and revenue_month between '2020-01-01' and '2020-12-31'
+GROUP BY pickup_zone
+ORDER BY total_revenue desc;
+
+-- Total trips for Green taxis in October 2019
+select
+	sum(total_monthly_trips)
+from taxi_rides_ny.analytics_prod.fct_monthly_zone_revenue
+where true
+	and service_type = 'Green'
+	and revenue_month between '2019-10-01' and '2019-10-31';
+
+-- Total records in stg_fhv_tripdata (filter dispatching_base_num IS NULL)
+
